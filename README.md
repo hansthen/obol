a308676515b1d6a50da1c9d1baa58682b54fe3e4
hansthen <hans.then@gmail.com>
2016-07-01 10:41:18 +0200

    Added setup scripts for pypi.

---
+++ README.md
@@ -0,0 +1,7 @@
+# Obol: useradd for ldap.
+
+Obol is a command line utility to add, delete and modify users and
+groups in an ldap server.
+
+© 2015 Hans Then. Obol is released under GPLv3.
+see `gpl.txt` for details.
---
+++ setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+setup(
+  name = 'obol',
+  scripts = ['obol'],
+  version = '1.0',
+  description = 'useradd for ldap',
+  author = 'Hans Then',
+  author_email = 'hans.then@gmail.com',
+  url = 'https://github.com/hansthen/obol', # use the URL to the github repo
+  download_url = 'https://github.com/hansthen/obol/tarball/1.0', # I'll explain this in a second
+  keywords = ['useradd', 'groupadd', 'ldap'], # arbitrary keywords
+  classifiers = [],
+  install_requires = [
+     'python-ldap',
+     'ldif3',
+     'retrying',
+  ]
+)
