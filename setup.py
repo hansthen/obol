9dda48c04d425f64b4263905eb54c460b3128ee7
hansthen <hans.then@gmail.com>
2016-07-06 19:18:57 +0200

    Converted README from markdown to RST.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
   name = 'obol',
-  version = '2.0',
+  version = '2.0.1',
   description = 'useradd for ldap',
   author = 'Hans Then',
   author_email = 'hans.then@gmail.com',
@@ -14,12 +14,12 @@ setup(
   download_url = 'https://github.com/hansthen/obol/tarball/2.0',
   keywords = ['useradd', 'groupadd', 'ldap'],
   license = 'GPLv3',
-  long_description=read('README.md'),
+  long_description=read('README.rst'),
   packages = ['obol'],
   classifiers=[
         "Development Status :: 4 - Beta",
         "Topic :: Utilities",
-        "License :: OSI Approved :: GPLv3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
   ],
   entry_points={
           'console_scripts': [
