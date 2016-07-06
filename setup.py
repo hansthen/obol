a94f7501307de0d193a6107c6fbe679f48f6d402
hansthen <hans.then@gmail.com>
2016-07-06 19:07:28 +0200

    Changed setup.py to use setuptools and improved .gitognore.

--- setup.py
+++ setup.py
@@ -1,18 +1,35 @@
-from distutils.core import setup
+from setuptools import setup
+import os
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
 setup(
   name = 'obol',
-  scripts = ['obol'],
-  version = '1.3',
+  version = '2.0',
   description = 'useradd for ldap',
   author = 'Hans Then',
   author_email = 'hans.then@gmail.com',
-  url = 'https://github.com/hansthen/obol', # use the URL to the github repo
-  download_url = 'https://github.com/hansthen/obol/tarball/1.3', # I'll explain this in a second
-  keywords = ['useradd', 'groupadd', 'ldap'], # arbitrary keywords
-  classifiers = [],
+  url = 'https://github.com/hansthen/obol',
+  download_url = 'https://github.com/hansthen/obol/tarball/2.0',
+  keywords = ['useradd', 'groupadd', 'ldap'],
+  license = 'GPLv3',
+  long_description=read('README.md'),
+  packages = ['obol'],
+  classifiers=[
+        "Development Status :: 4 - Beta",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: GPLv3",
+  ],
+  entry_points={
+          'console_scripts': [
+              'obol = obol.main:main'
+          ]
+  },
   install_requires = [
      'python-ldap',
      'ldif3',
      'retrying',
+     'cliff'
   ]
 )
