b7dac69903b60c0878950fde33870d3668a3f2b6
Hans Then <hans.then@gmail.com>
2016-07-19 21:59:27 +0000

    2.0.4 and added tag.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
   name = 'obol',
-  version = '2.0.3',
+  version = '2.0.4',
   description = 'useradd for ldap',
   author = 'Hans Then',
   author_email = 'hans.then@gmail.com',
